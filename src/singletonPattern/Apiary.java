045a5ff203097bb417b73963627ac1124ea10d88
JoshLSloan <jsloan13@asu.edu>
2018-11-22 13:23:02 -0700

    Apiary has been modified to have an ArrayList of Hive objects along with a method to add new hives and a toString method to print out the hive.
    A hive class has been added which just has an id number.
    
    These modifications will allow showing that Apiary behaves as a
    singleton should.

--- src/singletonPattern/Apiary.java
+++ src/singletonPattern/Apiary.java
@@ -1,16 +1,55 @@
 package singletonPattern;
 
+import java.util.ArrayList;
+
+/**
+ * A singleton that represents an Apiary which holds a list of Hives.
+ * Only one Apiary can ever exist.
+ * @author Joshua Sloan
+ *
+ */
 public class Apiary {
 
     private static Apiary singleApiary = null;
     
+    private ArrayList<Hive> hives = new ArrayList<>();
+    
     private Apiary() {}
     
+    /**
+     * Creates an returns an apiary instance if none exist.  Otherwise
+     * returns the existing Apiary.
+     * @return An apiary.
+     */
     public static Apiary getApiary() {
         if(singleApiary == null) {
             singleApiary = new Apiary();
         }
         
         return singleApiary;
+    }
+    
+    /**
+     * Adds a hive to the apiary with an id number if that number is not
+     * currently in use.
+     * @param parmId The id number of the new hive.
+     * @return True if the hive was added sucessfully, false otherwise.
+     */
+    public boolean addHive(int parmId) {
+        if (hives.contains(parmId)) {
+            return false; //id already in use
+        } else {
+            hives.add(new Hive(parmId));
+            return true;
+        }
+    }
+    
+    @Override
+    public String toString() {
+        String ret = "";
+        for (int i = 0; i < hives.size(); i++) {
+            ret += hives.get(i).toString() + "\n";
+        }
+        return ret;
     }
 }
---
+++ src/singletonPattern/Hive.java
@@ -0,0 +1,25 @@
+package singletonPattern;
+
+public class Hive {
+
+    private int id;
+    
+    public Hive(int parmId) {
+        this.id = parmId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    @Override
+    public String toString() {
+        return "ID: " + this.id;
+    }
+    
+    
+}
